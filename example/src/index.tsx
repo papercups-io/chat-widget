1242ece5871a5d7c0ff37afe8958ea6e6d7ca293
reichert621 <reichertjalex@gmail.com>
2020-08-14 14:10:45 -0400

    Allow overriding the iframe url

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,4 +4,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App disco />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
