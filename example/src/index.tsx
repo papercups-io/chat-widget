f06e469695012263518d4cc8e62ec2c3e5e843eb
Pascal Jufer <pascal-jufer@bluewin.ch>
2021-05-17 10:11:55 +0200

    Sync example with current create-react-app files, lint/cleanup code

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,7 +1,11 @@
-import './index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
